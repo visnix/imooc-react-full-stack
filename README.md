79f459cd97c0dbe600c5945c1914ab3bb66fb0d2
visnix <quantumprogr@gmail.com>
2020-09-11 11:18:24 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## 一个全栈的网页
+
+首先运行 `npm run server`
+然后运行 `npm start`
+最后访问 `http://localhost:3000/genius`去注册
--- server/model.js
+++ server/model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 // 链接mongo 并且使用imooc这个集合
 const DB_URL = 'mongodb://localhost:27017/imooc-chat'
+console.log('--------->')
 mongoose.connect(DB_URL)
 
 
